299a309cd254a2f5ef8c6977e8b3846cf53e284f
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-06-08 15:21:57 +0200

    added some samples

--- run.js
+++ run.js
@@ -1,6 +1,23 @@
+const fs = require('fs');
+const ncp = require('ncp');
 const nodemon = require('nodemon');
 
-nodemon({ script: 'index.js' });
+const prepare = () => new Promise(resolve => {
+  fs.exists('./panel-generated', exists => {
+    if (exists) resolve();
+    else {
+      console.log('==> POPULATING SAMPLES');
+      ncp('./samples', './panel-generated', () => resolve());
+    }
+  });
+});
 
-nodemon.on('start', () => console.log('starting the platform ...'));
-nodemon.on('crash', () => process.exit(0));
+const run = () => {
+  console.log('==> RUNNING PLATFORM');
+  nodemon({ script: 'index.js' });
+
+  nodemon.on('start', () => console.log('starting the platform ...'));
+  nodemon.on('crash', () => process.exit(0));
+}
+
+prepare().then(run);
